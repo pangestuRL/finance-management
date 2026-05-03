af93d24f6c4cd455f06a2f880d0782be8b573e48
pangestuRL <riskilestaripangestu@gmail.com>
2026-05-03 22:05:24 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Finance Management API' });
+});
+
+module.exports = app;
